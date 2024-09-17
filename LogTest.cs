713ae6ddc7653f55c60e313b5c2e53df21754b7e
Spoc Web <10584012+SpocWeb@users.noreply.github.com>
2024-09-17 22:22:43 +0200

    checking keys

--- LogTest.cs
+++ LogTest.cs
@@ -5,11 +5,11 @@ using Shouldly;
 namespace org.SpocWeb.root.Logging;
 
 //[TestSubject(typeof(Log))]
-public class LogTest
+public static class LogTest
 {
     private const string Expected = "Failed to post to ESB. \n MurexValue in ProductMaskfield";
 
-    readonly ChangedVariables _changedVariables = new()
+    static readonly ChangedVariables _changedVariables = new()
     {
         MurexValue = nameof(ChangedVariables.MurexValue),
         ProductMaskValue = nameof(ChangedVariables.ProductMaskValue),
@@ -17,18 +17,22 @@ public class LogTest
     };
 
     [Test]
-    public void TestParsePositional()
+    public static void TestParsePositional()
     {
         var log = Log.Parse($"Failed to post to ESB. \n {_changedVariables.MurexValue} in {_changedVariables.ProductMaskfield}");
         log.Values.Length.ShouldBe(2);
         log.ToString().ShouldBe(Expected);
-    }
+        var keys = log.ToDictionary().Keys.ToArray();
+		keys.ShouldBe([ "0", "1" ]);
+	}
 
     [Test]
-    public void TestParseNamed()
+    public static void TestParseNamed()
     {
         var log = Log.Parse(null, "Failed to post to ESB. \n {murexValue} in {productMaskField}", _changedVariables.MurexValue, _changedVariables.ProductMaskfield);
         log.Values.Length.ShouldBe(2);
         log.ToString().ShouldBe(Expected);
+        var keys = log.ToDictionary().Keys.ToArray();
+        keys.ShouldBe([ "murexValue", "productMaskField" ]);
     }
 }
